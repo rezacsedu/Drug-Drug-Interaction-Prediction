49073d9f97f9065b146c1f2b94314bc614a3e19f
Md. Rezaul Karim <reza_cse_du@yahoo.com>
2019-05-29 18:10:10 +0200

    Update DDI_prediction.py

--- DDI_prediction.py
+++ DDI_prediction.py
@@ -30,39 +30,48 @@ config.set("spark.executor.memory", "56g")
 config.set('spark.driver.memory', '30g')
 config.set("spark.memory.offHeap.enabled",True)
 config.set("spark.memory.offHeap.size","64g") 
-sc = SparkContext(conf=config)
+sc = SparkContext(conf = config)
 
-ddi_df = pd.read_csv("full_DDI.txt", sep='\t')
-ddi_df.head()
-
-featureFilename = "vectors/RDF2Vec/RDF2Vec_sg_300_5_5_15_2_500_d5_uniform.txt" 
-embedding_df = pd.read_csv(featureFilename, delimiter='\t') 
-
-embedding_df.Entity = embedding_df.Entity.str[-8:-1]
-embedding_df.rename(columns={'Entity':'Drug'}, inplace=True)
-
-len(set(ddi_df.Drug1.unique()).union(ddi_df.Drug2.unique()) )
-pairs, classes = ml.generatePairs(ddi_df, embedding_df)
-
-reg = L1L2(l1=0.01, l2=0.01)
-model = ml.Conv_LSTM(num_classes=2, timesteps=8, reg=reg)
+reg = L1L2(l1 = 0.01, l2 = 0.01)
+model = ml.Conv_LSTM(num_classes = 2, timesteps = 8, reg = reg)
 model.summary()
-convLSTM, history = ml.model_train(model, number_epoch=20)
+convLSTM = ml.model_train(model, number_epoch = 20)
 
-kNN = KNeighborsClassifier(n_neighbors=3)
+kNN = KNeighborsClassifier(n_neighbors = 3)
 NB = GaussianNB()
 SVM = svm.SVC()
-LR = linear_model.LogisticRegression(C=0.01)
-RF = ensemble.RandomForestClassifier(n_estimators=5, n_jobs=-1)
-GBT = ensemble.GradientBoostingClassifier(n_estimators=5, max_leaf_nodes=3,max_depth=3)
+LR = linear_model.LogisticRegression(C = 0.01)
+RF = ensemble.RandomForestClassifier(n_estimators = 5, n_jobs = -1)
+GBT = ensemble.GradientBoostingClassifier(n_estimators = 5, max_leaf_nodes = 3, max_depth = 3)
 
 clfs = [('LR',LR),('SVM',SVM),('NB',NB),('KNN',KNN),('GBT',GBT),('RF',LR),('Conv-LSTM Regression',convLSTM)]
 
-n_seed =100
-n_fold =5 
-n_run =10
-n_proportion = 1,
-all_scores_df = ml.kfoldCV(sc, pairs, classes, embedding_df, clfs, n_run, n_fold, n_proportion, n_seed)
 
-all_scores_df.groupby(['method','run']).mean().groupby('method').mean()
-all_scores_df.to_csv('Results.txt',sep='\t', index=False)
+def ddiDataGenerator(clfs, ddi_list, vectors):
+    ddi_df = pd.read_csv(ddi_list, sep='\t')
+
+    embedding_df = pd.read_csv(vectors, delimiter = '\t') 
+
+    embedding_df.Entity = embedding_df.Entity.str[-8:-1]
+    embedding_df.rename(columns = {'Entity':'Drug'}, inplace = True)
+
+    len(set(ddi_df.Drug1.unique()).union(ddi_df.Drug2.unique()) )
+    pairs, classes = ml.generatePairs(ddi_df, embedding_df)
+    
+    return pairs, classes, embedding_df
+
+ddi_list = 'full_DDI.txt'
+vectors = 'RDF2Vec_sg_300_5_5_15_2_500_d5_uniform.txt'
+
+pairs, classes, embedding_df = ddiDataGenerator(clfs, ddi_list, vectors)
+
+# Hyperparameters 
+n_seed = 100
+n_fold = 5 
+n_run = 10
+n_proportion = 1
+
+scoreDF = ml.kfoldCV(sc, pairs, classes, embedding_df, clfs, n_run, n_fold, n_proportion, n_seed)
+
+scoreDF.groupby(['method','run']).mean().groupby('method').mean()
+scoreDF.to_csv('Results.txt',sep = '\t', index = False)
